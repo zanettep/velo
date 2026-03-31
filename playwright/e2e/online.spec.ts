dbb2e6a9b50c0ce3b305060d254028119ebf91e5
Paulo Zanette <paulorzanette2018@gmail.com>
2026-03-31 13:43:15 -0300

    voltando para o título original

--- playwright/e2e/online.spec.ts
+++ playwright/e2e/online.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
-test('a webapp deve estar online', async ({ page }) => {
+test('webapp deve estar online', async ({ page }) => {
 	await page.goto('http://localhost:5173')
 
   	await expect(page).toHaveTitle(/Velô by Papito/)
