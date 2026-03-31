740481faa2fc5eda3924178b6fe0ce82372d23b4
Paulo Zanette <paulorzanette2018@gmail.com>
2026-03-31 13:41:23 -0300

    Mudando o título do cenário

--- playwright/e2e/online.spec.ts
+++ playwright/e2e/online.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test'
 
-test('webapp deve estar online', async ({ page }) => {
-	await page.goto('http://localhost:5173');
+test('a webapp deve estar online', async ({ page }) => {
+	await page.goto('http://localhost:5173')
 
   	await expect(page).toHaveTitle(/Velô by Papito/)
 })
